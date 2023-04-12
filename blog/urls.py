5b2f2937dc30968b3859ee528d4342e85ce3377d
Cameron Will <cameron@thewills.net>
2023-04-11 22:48:13 -0400

    chore: Change blog names to recipe names
    Change as many instances of the word "blog" as possible to "recipe" without
    renaming the app, which is enough of an effort as to require several different
    commits to do correctly without breaking anything and including the right
    migrations

--- blog/urls.py
+++ blog/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.PostListView.as_view(), name="blog-home"),
-    path("post/<int:pk>/", views.PostDetailView.as_view(), name="post-detail"),
-    path("post/new/", views.PostCreateView.as_view(), name="post-create"),
-    path("post/<int:pk>/update/", views.PostUpdateView.as_view(), name="post-update"),
-    path("post/<int:pk>/delete/", views.PostDeleteView.as_view(), name="post-delete"),
-    path("user/<str:username>/", views.UserPostListView.as_view(), name="user-posts"),
+    path("", views.RecipeListView.as_view(), name="recipe-home"),
+    path("recipe/<int:pk>/", views.RecipeDetailView.as_view(), name="recipe-detail"),
+    path("recipe/new/", views.RecipeCreateView.as_view(), name="recipe-create"),
+    path("recipe/<int:pk>/update/", views.RecipeUpdateView.as_view(), name="recipe-update"),
+    path("recipe/<int:pk>/delete/", views.RecipeDeleteView.as_view(), name="recipe-delete"),
+    path("user/<str:username>/", views.UserRecipeListView.as_view(), name="user-recipes"),
     path("about/", views.about, name="blog-about"),
 ]
